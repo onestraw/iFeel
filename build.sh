68f15876fb2bda1786b5e9a3c21349e9277fc29f
onestraw <hexiaowei91@163.com>
2015-07-11 22:57:23 -0700

    finish the script

--- build.sh
+++ build.sh
@@ -1,8 +1,45 @@
-#!/bin/sh
-#generate theadpool dynamic link library
-cd threadpool
-make
+#!/bin/bash
+#$cd=`which chdir`
+_help()
+{
+	echo "Usage: $1 [make|clean]"
+}
+_make()
+{
+	#generate theadpool dynamic link library
+	cd threadpool
+	make
+	
+	#build plugins
+	cd ../plugin
+	make
+	
+	cd  ..
+	make
+}
+_clean()
+{
+	cd threadpool
+	make clean
 
-#build plugins
-cd ../plugin
-make
+	cd ../plugin
+	make clean
+
+	cd ..
+	make distclean
+}
+
+_main()
+{
+	if [ "$1" = "make" ]; then
+		_make	
+	elif [ "$1" = "clean" ]; then
+		_clean	
+	else
+		_help $0
+		exit 1
+	fi
+	exit 0
+}
+
+_main $1
